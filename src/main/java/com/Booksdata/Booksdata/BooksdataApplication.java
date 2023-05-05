0d85a6532b3ff0d16f0eea45bc8efa58579f36c4
yogesh17999 <yogesh17999@gmail.com>
2023-05-05 14:58:03 +0530

    Books data for swagger

---
+++ src/main/java/com/Booksdata/Booksdata/BooksdataApplication.java
@@ -0,0 +1,13 @@
+package com.Booksdata.Booksdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksdataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Booksdata/Booksdata/BooksdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Booksdata.Booksdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
